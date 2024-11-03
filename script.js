3c705beb3a38058628155c270c48fc1804947cd5
SI-Tanbir <si017490@gmail.com>
2024-11-03 15:03:54 +0600

    adding seat number system in the app

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 
+
+
+// initalization of seat number
+
+let seat_number=0;
+
+
+
 document.querySelector(".button-container").addEventListener('click', (event) => {
 
     // console.log("button click ",event.target.innerText);
     // event.target.style.backgroundColor='green';
     // event.target.
 
+    // initializing value
+    let seat_price=750;
+    let seat_name=event.target.textContent;
 
     if(event.target.classList.contains('bus-button')){
 
 
 
+
+
         if(event.target.style.backgroundColor == 'green'){
             event.target.style.backgroundColor='';
+            seat_number--;
+
+
         
         }
 
         else{
             event.target.style.backgroundColor='green';
+            seat_number++;
+
         }
 
 
+        document.getElementById('seat-number').textContent=seat_number;
+        
+
+
     }
+
+
+
 
 
 
